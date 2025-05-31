16a0c05de36e78bfd158a944dc8722720f1c6862
Janviere-dev <j.munezero@student.com>
2025-05-31 12:15:45 +0300

    The simple intrest rate calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000
+rate = 0.05  # 5% interest rate
+time = 3  # time in years
+# Calculate simple interest
+interest = principal * rate * time
+# Print the result
+print(f"The simple intrest is :{interest}.")

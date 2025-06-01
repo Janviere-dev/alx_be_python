6ed7ab8e663a8f7ee7cab358312651fa5c42e0aa
Janviere-dev <j.munezero@student.com>
2025-06-01 21:48:11 +0300

    I change number to always be integer

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,3 @@
 number = int(input("Enter a number to see its multiplication table:"))
 for counter in range(1,11):
-    print(f"{number} x {counter} = {(number) * counter}")
+    print(f"{number} * {counter} = {(number) * counter}")
